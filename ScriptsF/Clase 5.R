c698bc7ef5510912174ca2732ca8a1d8661d5cca
1912963 <60185238+1912963@users.noreply.github.com>
2024-12-03 18:18:02 -0600

    20/09/2024

---
+++ ScriptsF/Clase 5.R
@@ -0,0 +1,57 @@
+
+# Correlación de datos  ---------------------------------------------------
+
+library(repmis)
+edad <- source_data("https://www.dropbox.com/s/nxoijhgmutuho0s/datos_control_Rascon.csv?dl=1")
+
+summary(edad)
+
+plot(edad$DAP, edad$EDAD, psh = 19, col = "indianred",
+     xlab = "Diámetro (cm)",
+     ylab = "Edad",
+     ylim = c(20,140),
+     xlim = c(10,50))
+
+cor.test(edad$DAP, edad$EDAD)
+ed.lm <- lm(edad$EDAD ~ edad$DAP)
+
+# Solo obtener el intercepto (alfa) y beta  -------------------------------
+
+
+ed.lm
+
+# Para obtener la significancia aplico summary  ---------------------------
+
+summary(ed.lm)
+
+plot(edad$DAP, edad$EDAD, psh = 19, col = "indianred",
+     xlab = "Diámetro (cm)",
+     ylab = "Edad",
+     ylim = c(20,140),
+     xlim = c(10,50))
+abline(ed.lm)
+text(20, 120, "y = -8.4 * 2.43(x)")
+ed.lm$coefficients
+ed.lm$residuals
+edad$res <- ed.lm$residuals
+edad$res <- ed.lm$fitted.values
+edad$com.res <- edad$EDAD ~ edad$edprim
+edad$edprim <- edad$EDAD
+edad$com.res <- edad$EDAD ~ edad$edprim
+
+# Suma residual -----------------------------------------------------------
+
+sum(edad$res)
+sum(edad$res^2)/58
+
+# Estimar la edad (prima) para los valores de DAP : 15, 30, 45, 47 --------
+
+valores <- c(15, 30, 45, 47)
+prima <- -8.4 + 2.4*(valores)
+
+
+
+
+
+plot(edad$EDAD, edad$DAP)
+
